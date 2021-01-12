ccd69af563bad42af6d55f8c1b7e13af8ec9a2c3
liyi <liyi2@yxrobot.com>
2021-01-12 17:07:54 +0800

    2
    2

--- app/src/main/java/com/aaa/lib/animationtest/NoNameAnimView.java
+++ app/src/main/java/com/aaa/lib/animationtest/NoNameAnimView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -13,32 +15,51 @@ import com.aaa.lib.animationtest.model.AnimPoint;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class NoNameAnimView extends View {
     private int maxPointCount = 1000;   //最大点的数量
+    private float maxLineLength = 150;   //最大连线距离
+    private float catchLength = 300;   //最大捕获半径
     private int maxLife = 100;      //点的生命上限， 每一次刷新增加1点 到最大值时 消失;
-    private int duration = 100;     //动画刷新间隔ms
+    private int duration = 16;     //动画刷新间隔ms
+    private int pointBornDuration = 1000;     //动画刷新间隔ms
     private volatile boolean show = false;  //是否在显示
 
     private Thread refreshThread;       //刷新线程
+    private Thread pointBornThread;       //生成点的线程
     private List<AnimPoint> animPoints;     // 点集
+    private List<AnimPoint> survivePoints;     // 存活的点集
+
+    private PointF touchPoint;
 
     private Paint pointPaint;       //画笔 用于绘制点
     private Paint pathPaint;        //画笔 用于绘制线
 
+    private float offsetX;
+    private float offsetY;
+
+
     public NoNameAnimView(Context context) {
         super(context);
+        init();
     }
 
     public NoNameAnimView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public NoNameAnimView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
         refreshThread = new Thread(new RefreshRunnable());
+        pointBornThread=new Thread(new PointBornRunnable());
+
         animPoints = new ArrayList<>();
+        survivePoints = new ArrayList<>();
 
         pointPaint = new Paint();
         pointPaint.setAntiAlias(true);
@@ -52,29 +73,87 @@ public class NoNameAnimView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+        removeDead();
         drawPoint(canvas);
         drawLine(canvas);
+    }
+
+    /**
+     *  移除老死的点 添加新点
+     */
+    private void removeDead(){
+        survivePoints.clear();
+        for(AnimPoint animPoint:animPoints){
+            if(!animPoint.isDead()){
+                survivePoints.add(animPoint);
+            }
+        }
+        animPoints.clear();
+        animPoints.addAll(survivePoints);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            touchPoint = new PointF(touchX, touchY);
+            offsetX = 0;
+            offsetY = 0;
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            offsetX += touchX - touchPoint.x;
+            offsetY += touchY - touchPoint.y;
+            touchPoint.set(touchX, touchY);
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            touchPoint = null;
+            offsetX = 0;
+            offsetY = 0;
+        }
+        return true;
     }
 
     private void drawPoint(Canvas canvas) {
         for (AnimPoint animPoint : animPoints) {
             // 更新point状态
-            animPoint.next(getWidth(),getHeight());
+            animPoint.next(getWidth(), getHeight(), touchPoint, catchLength, offsetX, offsetY);
             // draw point
             pointPaint.setColor(animPoint.color);
-            canvas.drawCircle(animPoint.x, animPoint.y, animPoint.radiu, pointPaint);
+            canvas.drawCircle(animPoint.x, animPoint.y, 6, pointPaint);
         }
-
+        offsetY = 0;
+        offsetX = 0;
     }
 
     private void drawLine(Canvas canvas) {
+        for (int i = 0; i < animPoints.size(); i++) {
+            AnimPoint pi = animPoints.get(i);
+            for (int j = i + 1; j < animPoints.size(); j++) {
+                AnimPoint pj = animPoints.get(j);
+                double distance = distance(pi, pj);
+                if (distance <= maxLineLength) {
+                    int color = Color.argb(255 - (int) (distance / maxLineLength * 255), 0, 0, 0);
+                    pathPaint.setColor(color);
+                    canvas.drawLine(pi.x, pi.y, pj.x, pj.y, pathPaint);
+                }
+            }
+            if (pi.isCatched) {
+                int color = Color.argb(pi.touchLineAlpha, 0, 0, 0);
+                pathPaint.setColor(color);
+                canvas.drawLine(pi.x, pi.y, touchPoint.x, touchPoint.y, pathPaint);
+            }
+        }
+    }
 
+    public double distance(AnimPoint p1, AnimPoint p2) {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
     }
 
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        init();
+        initPoint();
     }
 
     @Override
@@ -83,20 +162,9 @@ public class NoNameAnimView extends View {
         show = false;
     }
 
-    private void init() {
-        Random random = new Random();
+    private void initPoint() {
         for (int i = 0; i < 100; i++) {
-            float x = random.nextFloat() * 1000;
-            float y = random.nextFloat() * 1900;
-            float radiu = random.nextInt() * 10;
-            float speed = random.nextFloat() * 10 + 3;
-            float direction = random.nextFloat() * 360;
-            int a = random.nextInt() * 155 + 100;
-            int r = random.nextInt() * 255;
-            int g = random.nextInt() * 255;
-            int b = random.nextInt() * 255;
-            int color = Color.argb(a, r, g, b);
-            AnimPoint point = new AnimPoint(x, y, radiu, speed, direction, color);
+            AnimPoint point = new AnimPoint();
             animPoints.add(point);
         }
         show = true;
@@ -110,6 +178,21 @@ public class NoNameAnimView extends View {
             while (show) {
                 try {
                     Thread.sleep(duration);
+                    postInvalidate();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+    }
+    class PointBornRunnable implements Runnable {
+        @Override
+        public void run() {
+            while (show) {
+                try {
+                    Thread.sleep(pointBornDuration);
+
                     postInvalidate();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
--- app/src/main/java/com/aaa/lib/animationtest/model/AnimPoint.java
+++ app/src/main/java/com/aaa/lib/animationtest/model/AnimPoint.java
@@ -1,8 +1,12 @@
 package com.aaa.lib.animationtest.model;
 
-import java.lang.Math;
+import android.graphics.Color;
+import android.graphics.PointF;
+
+import java.util.Random;
 
 public class AnimPoint {
+    private final int escapeDistance = 10;   //逃逸速度
     public float x;
     public float y;
     public float radiu;
@@ -10,38 +14,94 @@ public class AnimPoint {
     public float direction;
     public int color;
     public int life;
+    public int maxLife;
+    public boolean isCatched;
+    public int touchLineAlpha;
 
-    public AnimPoint(float x, float y, float radiu,float speed, float direction, int color) {
+    public AnimPoint() {
+        Random random = new Random();
+        x = random.nextFloat() * 1000;
+        y = random.nextFloat() * 1900;
+        radiu = random.nextInt(10);
+        speed = random.nextFloat() * 2 + 1;
+        direction = random.nextFloat() * 360;
+        maxLife = random.nextInt(2000) + 500;
+        int a = random.nextInt(155) + 100;
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        color = Color.argb(a, r, g, b);
+    }
+
+    public AnimPoint(float x, float y, float radiu, float speed, float direction, int color) {
         this.x = x;
         this.y = y;
-        this.radiu=radiu;
+        this.radiu = radiu;
         this.speed = speed;
         this.direction = direction;
         this.color = color;
     }
-    public void next(float max_w,float max_h) {
-        x = x + Math.cos(speed);
-        y = y + Math.sin(speed);
-        boolean turnBack = false;
-        if (x >= max_w) {
-            x = max_w;
-            turnBack = true;
-        }
-        if (x <= 0) {
-            x = 0;
-            turnBack = true;
-        }
-        if (y >= max_h) {
-            y = max_h;
-            turnBack = true;
-        }
-        if (y <= 0) {
-            y = 0;
-            turnBack = true;
-        }
-        if (turnBack) {
+
+    public void next(float max_w, float max_h, PointF touchPoint, float catchLength, float touchOffX, float touchOffY) {
+        float tempX = x + (float) Math.cos(direction * 2 * Math.PI / 360) * speed;
+        float tempY = y + (float) Math.sin(direction * 2 * Math.PI / 360) * speed;
+        if (tempX >= max_w) {
+            tempX = max_w;
+            direction = 180 - direction;
+        } else if (tempX <= 0) {
+            tempX = 0;
             direction = 180 - direction;
         }
+        if (tempY >= max_h) {
+            tempY = max_h;
+            direction = 360 - direction;
+        } else if (tempY <= 0) {
+            tempY = 0;
+            direction = 360 - direction;
+        }
+        if (touchPoint == null) {
+            x = tempX;
+            y = tempY;
+            isCatched = false;
+        } else {
+            double distance = distance(tempX, tempY, touchPoint.x, touchPoint.y);
+            if (isCatched) {
+                if (distance < catchLength) {
+                    x = tempX;
+                    y = tempY;
+                } else {
+                    if (touchOffX > escapeDistance || touchOffY > escapeDistance) {
+                        //逃逸
+                        isCatched = false;
+                    } else {
+                        x += touchOffX;
+                        y += touchOffY;
+                    }
+                }
+                touchLineAlpha = 255 - (int) (distance / catchLength * 255);
+            } else {
+                // 是否捕获
+                if (distance <= catchLength) {
+                    isCatched = true;
+                    touchLineAlpha = 255 - (int) (distance / catchLength * 255);
+                }
+                x = tempX;
+                y = tempY;
+            }
+        }
+
         life++;
+    }
+
+    public static double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+    }
+
+    public boolean isDead() {
+        if (life > maxLife) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
